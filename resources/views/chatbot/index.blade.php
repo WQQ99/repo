9acc959fcbdbc8f00988548e1e65b5840aa8de2d
Lloyd Yeo <ywz.lloyd@gmail.com>
2018-02-01 01:53:31 +0800

    Web: Added javascript for chatting

--- resources/views/chatbot/index.blade.php
+++ resources/views/chatbot/index.blade.php
@@ -23,7 +23,8 @@
 		<div class="br-pagebody">
 
 			<div class="row row-sm mg-t-20">
-				<div class="col-lg-12 col-xl-12 mg-t-20 mg-lg-t-0">
+				<div class="col-lg-3"></div>
+				<div class="col-lg-6 mg-t-20 mg-lg-t-0">
 					<div class="card shadow-base widget-6">
 						<div class="card-header">
 							<div class="media align-items-center">
@@ -34,32 +35,32 @@
 									<span class="tx-12">Active now</span>
 								</div><!-- media-body -->
 							</div><!-- media -->
-							<span class="tx-12">
-                  {{--<a href="" class="tx-gray mg-r-10">Chat History</a>--}}
-                  {{--<a href="" class="tx-gray mg-r-10">Report</a>--}}
-                  {{--<a href="" class="tx-gray"><i class="fa fa-angle-down tx-14"></i></a>--}}
-                </span>
+							<span class="tx-12"></span>
 						</div><!-- card-header -->
 						<div class="card-body pd-25">
-							<div class="media-list">
-
-								<div class="media mg-t-20">
+							<div class="media-list" id="chat-thread">
+								<div class="media mg-t-20 response">
 									<img src="http://via.placeholder.com/280x280" class="wd-36 rounded-circle mg-r-20"
 									     alt="">
 									<div class="media-body tx-12">
-                                                                            <div class="chat-1">
-										Hi there, how can i help you?
-                                                                                <br/>
-                                                                                <br/>
-                                                                                <button class="mg-t-28 btn btn-sm  btn-outline-primary" style="font-size: 13px;">Browsing for item</button>
-                                                                                <br/>
-                                                                                <button class="mg-t-28 btn btn-sm  btn-outline-primary" style="font-size: 13px;">Request for item</button>
-                                                                            </div>
+										<div class="chat-1">
+											Hi there, how can i help you?
+											<br/>
+											<button class="mg-t-10 btn btn-sm btn-outline-primary selection-response-btn"
+											        data-response="Browsing for Item"
+											        style="font-size: 13px;">Browsing for Item
+											</button>
+											<br/>
+											<button class="mg-t-10 btn btn-sm btn-outline-primary selection-response-btn"
+											        data-response="Request for Item"
+											        style="font-size: 13px;">Request for Item
+											</button>
+										</div>
 										<span class="chat-time">3:00pm</span>
 									</div><!-- media-body -->
 								</div><!-- media -->
-                                                                
-                                                                <div class="media mg-t-20">
+
+								<div class="media mg-t-20" style="display:none;">
 									<img src="http://via.placeholder.com/280x280" class="wd-36 rounded-circle mg-r-20"
 									     alt="">
 									<div class="media-body tx-12">
@@ -67,24 +68,23 @@
 											I'm browsing for items !
 										</div>
 										<span class="chat-time">3:10pm</span>
-										
-									</div><!-- media-body -->
-								</div><!-- media -->
-                                                                
-                                                                <div class="media mg-t-20">
+									</div>
+								</div>
+
+								<div class="media mg-t-20" style="display:none;">
 									<img src="http://via.placeholder.com/280x280" class="wd-36 rounded-circle mg-r-20"
 									     alt="">
 									<div class="media-body tx-12">
-                                                                            <div class="chat-1">
-										What items are you looking for?
-                                                                                
-                                                                                <br/>                                                                        
-                                                                            </div>
+										<div class="chat-1">
+											What items are you looking for?
+
+											<br/>
+										</div>
 										<span class="chat-time">3:00pm</span>
 									</div><!-- media-body -->
 								</div><!-- media -->
 
-								<div class="media mg-t-20">
+								<div class="media mg-t-20" style="display:none;">
 									<img src="http://via.placeholder.com/280x280" class="wd-36 rounded-circle mg-r-20"
 									     alt="">
 									<div class="media-body tx-12">
@@ -109,7 +109,7 @@
 									</div><!-- media-body -->
 								</div><!-- media -->
 
-								<div class="media mg-t-20">
+								<div class="media mg-t-20" style="display:none;">
 									<img src="http://via.placeholder.com/280x280" class="wd-36 rounded-circle mg-r-20"
 									     alt="">
 									<div class="media-body tx-12">
@@ -129,6 +129,32 @@
 									</div><!-- media-body -->
 								</div><!-- media -->
 
+								<div id="response-template" style="display:none;">
+									<div class="media mg-t-20 response-last">
+										<img src="http://via.placeholder.com/280x280" class="wd-36 rounded-circle mg-r-20"
+										     alt="">
+										<div class="media-body tx-12">
+											<div class="chat-2">
+												I'm browsing for items !
+											</div>
+											<span class="chat-time">3:10pm</span>
+										</div>
+									</div>
+								</div>
+
+								<div id="chatbot-response-template" style="display:none;">
+									<div class="media mg-t-20 response-last">
+										<img src="http://via.placeholder.com/280x280" class="wd-36 rounded-circle mg-r-20"
+										     alt="">
+										<div class="media-body tx-12">
+											<div class="chat-1">
+
+											</div>
+											<span class="chat-time">3:10pm</span>
+										</div>
+									</div>
+								</div>
+
 							</div><!-- media-list -->
 						</div><!-- card-body -->
 						<div class="card-footer">
@@ -136,7 +162,7 @@
 								<img src="http://via.placeholder.com/280x280" class="wd-36 rounded-circle mg-r-20"
 								     alt="">
 								<div class="media-body">
-									<input type="text" class="form-control tx-12" placeholder="Type your message">
+									<input type="text" class="form-control tx-12" id="chatbot-input-txtbox" placeholder="Type your message">
 								</div><!-- media-body -->
 							</div><!-- media -->
 						</div><!-- card-footer -->
@@ -146,6 +172,42 @@
 		</div>
 	</div>
 
+	<script>
+
+
+		$('body').on('click', ".selection-response-btn", function(){
+			$response = $(this).data('response');
+
+            $template_text_elem = $("#response-template > div > div > div.chat-2");
+            $template_text_elem.html($response);
+
+            $("#chat-thread").html($("#chat-thread").html() + $("#response-template").html());
+
+			console.log($response);
+
+            if ($response == "Browsing for Item") {
+
+                $chatbot_template_text_elem = $("#chatbot-response-template > div > div > div.chat-1");
+                $chatbot_template_text_elem.html("What category of items will you be looking for?");
+                $("#chat-thread").html($("#chat-thread").html() + $("#chatbot-response-template").html());
+
+            } else if ($response == "Request for Item") {
+
+            }
+		});
+
+        $("#chatbot-input-txtbox").keypress(function(event){
+            if ( event.which == 13 ) {
+                event.preventDefault();
+                $response = $(this).val();
+                console.log($response);
+                $template_text_elem = $("#response-template > div > div > div.chat-2");
+                $template_text_elem.html($response);
+                $("#chat-thread").html($("#chat-thread").html() + $("#response-template").html());
+				$(this).val("");
+            }
+        });
+	</script>
 
 
 @endsection
